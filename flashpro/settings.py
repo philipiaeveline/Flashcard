cc42d119ab76523e78fc9795042b13cd3d448bc3
philipiaeveline <philipiaeveline13@gmail.com>
2021-01-28 12:39:23 +0300

    add

--- flashpro/settings.py
+++ flashpro/settings.py
@@ -75,11 +75,12 @@ WSGI_APPLICATION = 'flashpro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'flash',
+        'USER': 'moringa',
+    'PASSWORD':'dellakomar24',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
